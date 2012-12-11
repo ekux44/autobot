2c4d46ee537bef27f72efdf1efbe499543e073e1
ekux44 <ekux44@gmail.com>
2012-12-11 13:51:06 -0600

    Added AlchNotedInventory placeholder

---
+++ src/AlchNotedInventory.java
@@ -0,0 +1,22 @@
+
+public class AlchNotedInventory extends Behavior {
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean canAct() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
